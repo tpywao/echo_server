1f43c684784265ebc0624fd52271c528e974f3a0
tpywao <tpywao@gmail.com>
2018-01-18 20:42:40 +0900

    config, handlerバージョン

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,56 @@
-use std::io;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
-use std::net::{SocketAddr};
+use std::net::SocketAddr;
 use std::thread;
 
-fn handle(mut stream: TcpStream) -> io::Result<()> {
-    let mut buf = [0; 64];
-    loop {
-        let len = stream.read(&mut buf).unwrap();
-        if len == 0 {
-            println!("Client Closed.");
-            return Ok(())
-        } else {
-            stream.write(&buf[..len]).unwrap();
-        }
+trait Handler: Send + Copy + 'static {
+    fn handle(self, &mut TcpStream);
+}
+
+fn run_server<H: Handler>(config: Config<H>) {
+    let listener = TcpListener::bind(config.host).unwrap();
+    for stream in listener.incoming() {
+        thread::spawn(move || {
+            config.handler.handle(
+                &mut stream.unwrap())
+        });
     }
 }
 
-struct EchoServer {
+#[derive(Clone, Copy)]
+struct Config<H> {
     host: SocketAddr,
+    handler: H,
 }
-impl EchoServer {
-    fn new(host: &str) -> EchoServer {
-        let host = host.parse().unwrap();
-        EchoServer {
-            host: host,
+impl<H: Handler> Config<H> {
+    fn new(host: &str, handler: H) -> Config<H> {
+        Config {
+            // 不明点: parseでlocalhost:8080が解析できないこと
+            host: host.parse().unwrap(),
+            handler,
         }
     }
+}
 
-    fn listen(self) {
-        let listener = TcpListener::bind(self.host).unwrap();
-        for stream in listener.incoming() {
-            thread::spawn(move || {
-                handle(stream.unwrap())
-            });
+#[derive(Clone, Copy)]
+struct Echo;
+impl Handler for Echo {
+    fn handle(self, stream: &mut TcpStream) {
+        let mut buf = [0; 64];
+        loop {
+            let len = stream.read(&mut buf).unwrap();
+            if len == 0 {
+                println!("Client Closed.");
+                break;
+            } else {
+                stream.write(&buf[..len]).unwrap();
+            }
         }
     }
 }
 
 fn main() {
     let host = "127.0.0.1:8080";
-    let server = EchoServer::new(host);
-    server.listen();
+    let config = Config::new(host, Echo);
+    run_server(config);
 }
